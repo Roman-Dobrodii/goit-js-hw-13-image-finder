93fb6f53df1c6bd7f8b3d25e5e7a3158a05f7509
Roman Dobrodii <romariosi007@gmail.com>
2020-11-21 23:44:21 +0200

    ДЗ

---
+++ src/js/apiService.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default {
+  page: 1,
+  query: ' ',
+  async fetchArticles() {
+    const keyapi = '19186547-e8c2926af7125cb35de57caef';
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${keyapi}`;
+    const res = await fetch(url);
+    const parseRes = await res.json();
+    this.incrementPage();
+    return parseRes.hits;
+  },
+  get searchQuerry() {
+    return this.query;
+  },
+  set searchQuerry(string) {
+    this.query = string;
+  },
+  incrementPage() {
+    this.page += 1;
+  },
+  resetPage() {
+    this.page = 1;
+  },
+};
---
+++ src/js/imageSearch.js
@@ -0,0 +1,65 @@
+import servise from './apiService';
+import cardImage from '../templates/cardTpl.hbs';
+import refs from './refs';
+
+const debounce = require('lodash.debounce');
+
+refs.searchForm.addEventListener(
+  'input',
+  debounce(imageSearchInputHandler, 500),
+);
+refs.loadMoreBtn.addEventListener('click', loadMoreBtnHandler);
+
+function imageSearchInputHandler(e) {
+  e.preventDefault();
+  servise.searchQuerry = e.target.value;
+  if (servise.query === '') {
+    hiddenLoadMoreBtn();
+    clearListItems();
+    return;
+  }
+
+  clearListItems();
+  servise.resetPage();
+
+  servise.fetchArticles().then(hits => {
+    const markup = buildListItemsTemplate(hits);
+    insertListItems(markup);
+  });
+  noHiddenLoadMoreBtn();
+}
+
+function loadMoreBtnHandler() {
+  refs.loadMoreBtn.disabled = true;
+  servise.fetchArticles().then(hits => {
+    const markup = buildListItemsTemplate(hits);
+    insertListItems(markup);
+    refs.loadMoreBtn.disabled = false;
+    window.scrollBy(0, 1000);
+
+    window.scrollBy({
+      top: 1000,
+      behavior: 'smooth',
+    });
+  });
+}
+
+function insertListItems(items) {
+  refs.gallery.insertAdjacentHTML('beforeend', items);
+}
+
+function buildListItemsTemplate(items) {
+  return cardImage(items);
+}
+
+function clearListItems() {
+  refs.gallery.innerHTML = '';
+}
+
+function hiddenLoadMoreBtn() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
+
+function noHiddenLoadMoreBtn() {
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
